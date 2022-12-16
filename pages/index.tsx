a76aef7073ae1b313f1ef2645d9db6bedac3fc83
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2022-12-16 12:56:28 +0530

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,6 +54,28 @@ export default function Home() {
       distance: "2-hour drive",
     },
   ];
+  const cards = [
+    {
+      id:1,
+      img:"https://links.papareact.com/2io",
+      title:"Outdoor getaways"
+    },
+    { 
+      id:2,
+      img:"https://links.papareact.com/q7j",
+      title:"Unique stays"
+    },
+    {
+      id:3,
+      img:"https://links.papareact.com/s03",
+      title:"Entire homes"
+    },
+    {
+      id:4,
+      img:"https://links.papareact.com/8ix",
+      title:"Pet allowed"
+    }
+  ];
   return (
     <div>
       <Head>
@@ -94,7 +116,26 @@ export default function Home() {
           </div>
         </section>
 
-        
+        <section>
+          <h2 className="text-4xl font-semibold py-8 text-red-500">Live Anywhere</h2>
+
+          <div className="flex space-x-4 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cards.map(({id,img,title})=>
+            (
+              <div 
+              key={id}
+              className='cursor-pointer hover:scale-105 transition transform duration-300 ease-out '>
+                <div className="relative h-80 w-80">
+                    <Image src={img} alt="destination"
+                    layout="fill"
+                    className="rounded-xl " />
+                </div>
+                <h3 className="text-2xl mt-3">{title}</h3>
+              </div>
+            ))}
+          </div>
+
+        </section>
       </main>
     </div>
   );
